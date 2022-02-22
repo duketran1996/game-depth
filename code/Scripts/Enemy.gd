2dced98e4e3d7ce487988b670e0215a8eba866d6
SEEXPENG <97987421+SEEXPENG@users.noreply.github.com>
2022-02-21 22:22:59 -0500

    Enemy updated

---
+++ code/Scripts/Enemy.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+export var direction = -1
+export var detets_cliff = true
+
+func _reay():
+	if direction == 1:
+		$AnimatedSprite.flip_h = true
+
+func _physics_process(delta):
+	if is_on_wall():
+		direction = direction*-1
+		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
+	velocity.y+= 20
+	velocity.x = 50 * direction
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
