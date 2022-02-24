76ac8a92680b9e88c2b2103704720b99718eb2c0
SEEXPENG <97987421+SEEXPENG@users.noreply.github.com>
2022-02-24 15:52:29 -0500

    Optimized sword

--- code/Scripts/SwordLight.gd
+++ code/Scripts/SwordLight.gd
@@ -1,13 +1,18 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var damage = 30
+
+func _ready():
+	$Timer.start()
+
 
 func _on_SwordLight_body_entered(body):
 	if "Enemy" in body.name:
 		body.health -= damage
 	if body:
 		queue_free()
+
+
+
+func _on_Timer_timeout():
+	queue_free()
